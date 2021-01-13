3b0ad9e165fc3132bc8adf3ca7b05b817153e44b
ChloeArd <chloe.ardoise@gmail.com>
2021-01-13 16:08:31 +0100

    Memory

--- game.js
+++ game.js
@@ -2,50 +2,64 @@ let win = 0;
 let score = document.getElementById("score");
 let cards = document.getElementsByClassName("case");
 let randomCards = cards[Math.floor(Math.random() * cards.length)]; //Supprime ???????????????????
-let cardss = ["case1", "case2", "case3", "case4", "case5", "case6", "case7", "case8", "case9", "case10", "case11", "case12"];
+let cardss = ["case1", "case2", "case3", "case4", "case5", "case6", "case7", "case8", "case9", "case10", "case11", "case12"]
+let images = [
+    "https://cdn.discordapp.com/attachments/689017273050202134/798839709166075925/unknown.png", //image spider-man
+    "https://cdn.discordapp.com/attachments/689017273050202134/798839709166075925/unknown.png", //image spider-man
+    "https://cdn.discordapp.com/attachments/689017273050202134/798845534882234368/unknown.png", //image iron man
+    "https://cdn.discordapp.com/attachments/689017273050202134/798845534882234368/unknown.png", //image iron man
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846022863814696/unknown.png", //image captain america
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846022863814696/unknown.png", //image captain america
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846246017695764/unknown.png", //image thor
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846246017695764/unknown.png", //image thor
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846526813110302/unknown.png", //image hawkeye
+    "https://cdn.discordapp.com/attachments/689017273050202134/798846526813110302/unknown.png", //image hawkeye
+    "https://cdn.discordapp.com/attachments/689017273050202134/798847407563079720/unknown.png", //image black widow
+    "https://cdn.discordapp.com/attachments/689017273050202134/798847407563079720/unknown.png"  //image black widow
+]
 let randomCardss;
+let randomImages;
 let image = document.getElementsByTagName("img");
 let nbClick = 0;
-
-
+let flippedCards = [];
 
 //place the pictures randomly in the 12 cards
 for(let i = 0; i < 12; i++){
     if (i === 0){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798839709166075925/unknown.png"); //image spider-man
+        imageCards();
     }
     else if (i === 1){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798839709166075925/unknown.png"); //image spider-man
+        imageCards();
     }
     else if (i === 2){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798845534882234368/unknown.png"); //image iron man
+        imageCards();
     }
     else if (i === 3){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798845534882234368/unknown.png"); //image iron man
+        imageCards();
     }
     else if (i === 4){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846022863814696/unknown.png" ); //image captain america
+        imageCards();
     }
     else if (i === 5){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846022863814696/unknown.png" ); //image captain america
+        imageCards();
     }
     else if (i === 6){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846246017695764/unknown.png" ); //image thor
+        imageCards();
     }
     else if (i === 7){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846246017695764/unknown.png" ); //image thor
+        imageCards();
     }
     else if (i === 8){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846526813110302/unknown.png" ); //image hawkeye
+        imageCards();
     }
     else if (i === 9){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798846526813110302/unknown.png" ); //image hawkeye
+        imageCards();
     }
     else if (i === 10){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798847407563079720/unknown.png" ); //image black widow
+        imageCards();
     }
     else if (i === 11){
-        imageCards("https://cdn.discordapp.com/attachments/689017273050202134/798847407563079720/unknown.png" ); //image black widow
+        imageCards();
     }
 }
 
@@ -79,6 +93,7 @@ for (let i = 0; i < cards.length; i++){
             clickCards(10);
             clickCards(11);
             nbClick++;
+            flippedCards.push(clickCards(i));
         }
         else if(nbClick === 2 ){
             for (let i = 0; i < image.length; i++) {
@@ -90,14 +105,17 @@ for (let i = 0; i < cards.length; i++){
 }
 
 //create an image with a url in a cards randomly and delete the id on cardss, for no repeat
-function imageCards(URL){
+function imageCards(){
     randomCardss = cardss[Math.floor(Math.random() * cardss.length)];
+    randomImages = images[Math.floor(Math.random() * images.length)];
     let createimg = document.createElement("img");
-    createimg.src = URL;
-    let index = cardss.indexOf(randomCardss);
+    createimg.src = randomImages;
+    let indexCardss = cardss.indexOf(randomCardss);
+    let indexImages = images.indexOf(randomImages);
     document.getElementById(randomCardss).append(createimg);
     //delete the word on array cardss
-    cardss.splice(index, 1);
+    cardss.splice(indexCardss, 1);
+    images.splice(indexImages, 1)
 }
 
 // when you click on a card, its image appears
@@ -105,9 +123,7 @@ function clickCards(index){
     for (let i = index; i < cards.length; i++){
         cards[index].addEventListener("click", function () {
             for (let i = index; i < image.length; i++) {
-                if (event = "click") {
                     image[index].style.display = "block";
-                }
             }
         });
     }
